6661cbda0b9493d83eafb2ccacd76c9a6553d4f4
Pascal Poizat <pascal.poizat@lip6.fr>
2016-09-16 17:32:21 +0200

    Add support for 2 distant sites

---
+++ src/run-jekyll-local-as-github.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+bundle exec jekyll serve --config _config.yml,_config_github.yml
--- src/update-site-github.sh
+++ src/update-site-github.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
+bundle exec jekyll build --config _config.yml,_config_github.yml
 # from https://github.com/rudametw/rudametw.github.io
-bundle exec jekyll build --config _config.yml
 'cp' -aRf _site/* ../ && git add ../ && git commit -m "Update site" && git push
